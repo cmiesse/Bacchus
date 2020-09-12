23b2a5e4ff8d3334adda943d47d18b5959cea236
cmiesse <c.miesse@students.ephec.be>
2020-09-12 16:23:05 +0200

    version fonctionnelle pour blog

--- blog/models.py
+++ blog/models.py
@@ -5,13 +5,13 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    titre = models.CharField(max_length=100)
+    contenu = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return self.titre
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
--- blog/views.py
+++ blog/views.py
@@ -23,14 +23,14 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 5
+    #paginate_by = 5
 
 
 class UserPostListView(ListView):
     model = Post
     template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
-    paginate_by = 5
+    #paginate_by = 5
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
@@ -43,7 +43,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['titre', 'contenu']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -52,7 +52,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['titre', 'contenu']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
